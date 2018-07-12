fe9fd55d0d4eac3c69b15c42f3f14ac5a2b3b9a1
Itanael <cynra.sheron@laposte.net>
2018-07-12 11:19:56 +0200

    add Form Inscription

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
 {
@@ -16,4 +21,27 @@ class UserController extends Controller
             'controller_name' => 'UserController',
         ]);
     }
+
+
+    public function inscription(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('inscription');
+        }
+
+        return $this->render('user/inscription.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+   
 }
